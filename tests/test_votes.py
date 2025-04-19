60c4bf4d94674c0d0de57f0c6467760a273a368a
gowtham <gowthamsade9@gamil.com>
2025-04-19 18:52:23 -0400

    changed code

--- tests/test_votes.py
+++ tests/test_votes.py
@@ -9,11 +9,11 @@
 #     session.commit()
 
 
-# def test_vote_on_post(authorized_client, test_posts):
-#     res = authorized_client.post(
-#         "/vote/", json={"post_id": test_posts[3].id, "dir": 1}
-#     )
-#     assert res.status_code == 201
+def test_vote_on_post(authorized_client, test_posts):
+    res = authorized_client.post(
+        "/vote/", json={"post_id": test_posts[3].id, "dir": 1}
+    )
+    assert res.status_code == 201
 
 
 # def test_vote_twice_post(authorized_client, test_posts, test_vote):
