687e18a29a18706a3145ecb6e9c2ef34cf369a68
gowtham <gowthamsade9@gamil.com>
2025-04-20 22:07:56 -0400

    add

--- app/models.py
+++ app/models.py
@@ -1,11 +1,18 @@
 from .database import Base
 from sqlalchemy import Column, Integer, String, Boolean, TIMESTAMP, text, ForeignKey
-from sqlalchemy.sql.sqltypes import TIMESTAMP
 from sqlalchemy.orm import relationship
-
-from sqlalchemy import Column, Integer, String, TIMESTAMP
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.sql import func
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True, index=True, nullable=False)
+    password = Column(String, nullable=False)
+    created_at = Column(TIMESTAMP(timezone=True), server_default=func.now())
+
+    posts = relationship("Post", back_populates="owner")
 
 
 class Post(Base):
@@ -20,34 +27,13 @@ class Post(Base):
 
     owner_id = Column(Integer, ForeignKey(
         "users.id", ondelete="CASCADE"), nullable=False)
-
-    owner = relationship("User")
-
-
-class User(Base):
-    __tablename__ = "users"
-    id = Column(Integer, primary_key=True, nullable=False)
-    email = Column(String, nullable=False, unique=True)
-    password = Column(String, nullable=False)
-    created_at = Column(TIMESTAMP(timezone=True),
-                        nullable=False, server_default=text('now()'))
+    owner = relationship("User", back_populates="posts")
 
 
 class Vote(Base):
     __tablename__ = "votes"
+
     user_id = Column(Integer, ForeignKey(
         "users.id", ondelete="CASCADE"), primary_key=True)
     post_id = Column(Integer, ForeignKey(
-        "users.id", ondelete="CASCADE"), primary_key=True)
-
-
-Base = declarative_base()
-
-
-class User(Base):
-    __tablename__ = "users"
-
-    id = Column(Integer, primary_key=True, index=True)
-    email = Column(String, unique=True, index=True, nullable=False)
-    password = Column(String, nullable=False)
-    created_at = Column(TIMESTAMP(timezone=True), server_default=func.now())
+        "posts.id", ondelete="CASCADE"), primary_key=True)
